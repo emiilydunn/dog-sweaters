7e8d43e9e82a87d136ac123f50038953c0b2f1e6
Emily Dunn <emilydunn13@gmail.com>
2024-11-15 10:02:54 -0400

    installed bcrypt

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,6 +4,24 @@ import multer from 'multer';
 const router = express.Router();
 
 //Set up Multer for image handling 
+
+//Multer setup to upload images
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/images/'); // save uploaded files in `public/images` folder
+    },
+    filename: function (req, file, cb) {
+      //Get file extension
+      const ext = file.originalname.split('.').pop();
+      //Generate unique filename - current timestamp + random number between 0 and 1000.
+      const uniqueFilename = Date.now() + '-' + Math.round(Math.random() * 1000) + '.' + ext;
+      cb(null, uniqueFilename);
+    }
+  });
+  const upload = multer({ storage: storage });
+
+
+
 //Routes
 //Get all products
 router.get('/all', (req, res) => {
